219b2223f791470ea63b542d5114b329f0443020
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 01:48:45 +0300

    Create scraper_trendyol.py

---
+++ scraper_trendyol.py
@@ -0,0 +1,10 @@
+def scrape_trendyol(url):
+    return {
+        "name": "Xiaomi Mi Smart Air Fryer",
+        "price": "3.499 TL",
+        "segment": "Orta",
+        "reviews": [
+            "Patates harika oluyor.", "Biraz küçük ama yeterli."
+        ],
+        "expert_score": 80
+    }
