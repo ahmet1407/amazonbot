a3ee2a4a3cf5d04223726d8a61c1b3e1505687a8
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 01:48:31 +0300

    Create scraper_hepsiburada.py

---
+++ scraper_hepsiburada.py
@@ -0,0 +1,10 @@
+def scrape_hepsiburada(url):
+    return {
+        "name": "Arzum Okka Minio",
+        "price": "1.799 TL",
+        "segment": "Orta",
+        "reviews": [
+            "Kahveyi hızlı yapıyor.", "Köpük bol, ama hazne küçük."
+        ],
+        "expert_score": 78
+    }
