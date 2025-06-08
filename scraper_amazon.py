5db6ccaaa712dec7e46abb54272e3a341a8a3ed2
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 01:48:18 +0300

    Create scraper_amazon.py

---
+++ scraper_amazon.py
@@ -0,0 +1,10 @@
+def scrape_amazon(url):
+    return {
+        "name": "Dyson V15 Detect",
+        "price": "18.999 TL",
+        "segment": "Premium",
+        "reviews": [
+            "Çekim gücü çok iyi", "LED ışıklı başlık harika", "Fiyatı pahalı ama değer"
+        ],
+        "expert_score": 94
+    }
