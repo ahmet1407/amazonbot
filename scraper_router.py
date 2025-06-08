25bc9b8849312dbcff2b2caeeeb092882ec0d9f9
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 01:46:43 +0300

    Update and rename scraper_amazon.py to scraper_router.py

---
+++ scraper_router.py
@@ -0,0 +1,13 @@
+from scraper_amazon import scrape_amazon
+from scraper_hepsiburada import scrape_hepsiburada
+from scraper_trendyol import scrape_trendyol
+
+def scrape_link(url):
+    if "amazon.com.tr" in url:
+        return scrape_amazon(url)
+    elif "hepsiburada.com" in url:
+        return scrape_hepsiburada(url)
+    elif "trendyol.com" in url:
+        return scrape_trendyol(url)
+    else:
+        raise ValueError("Desteklenmeyen link. Sadece Amazon, Hepsiburada ve Trendyol destekleniyor.")
