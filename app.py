dd122886814a6587523740e069aa64745e347e80
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 04:33:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 from flask import Flask, request, jsonify, Response
 from scraper_router import scrape_link
 from score_engine import generate_scorecard
