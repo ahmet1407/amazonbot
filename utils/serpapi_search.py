680dd15ac6f5d1e9896a408ab084d8bbb33ee291
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 03:55:44 +0300

    Update serpapi_search.py

--- utils/serpapi_search.py
+++ utils/serpapi_search.py
@@ -1,25 +1,10 @@
-import os
-import requests
-from urllib.parse import urlencode
+from core import app
+from scraper_manager import handle_scraping
+from flask import request
 
-SERPAPI_KEY = os.getenv("SERPAPI_KEY")
-
-def perform_serpapi_search(url: str) -> dict:
-    if not SERPAPI_KEY:
-        raise ValueError("SERPAPI_KEY environment variable not set.")
-
-    params = {
-        "engine": "google",
-        "q": url,
-        "api_key": SERPAPI_KEY,
-        "device": "desktop",
-        "num": "10"
-    }
-
-    endpoint = f"https://serpapi.com/search.json?{urlencode(params)}"
-    response = requests.get(endpoint)
-    
-    if response.status_code != 200:
-        raise Exception(f"SerpAPI error: {response.status_code}, {response.text}")
-
-    return response.json()
+@app.route("/scrape", methods=["POST"])
+def scrape_link():
+    data = request.get_json()
+    link = data.get("link")
+    result = handle_scraping(link)
+    return result
