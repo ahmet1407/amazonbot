9cf5571a1cd5db7fe058ea3bcbdf9c963145ec42
ahmet1407 <59695777+ahmet1407@users.noreply.github.com>
2025-06-09 03:15:34 +0300

    Update scraper_router.py

--- scraper_router.py
+++ scraper_router.py
@@ -5,21 +5,11 @@ from scraper_trendyol import scrape_trendyol
 def scrape_link(input_text):
     text = input_text.lower()
 
-    # Linkse kontrol et
     if "amazon" in text:
         return scrape_amazon(input_text)
     elif "hepsiburada" in text:
         return scrape_hepsiburada(input_text)
     elif "trendyol" in text:
         return scrape_trendyol(input_text)
-
-    # İsimse yönlendir
-    elif "dyson" in text and "v15" in text:
-        return scrape_amazon("Dyson V15 Detect")
-    elif "supersonic" in text or "kurutma" in text:
-        return scrape_hepsiburada("Dyson Supersonic Nural")
-    elif "air fryer" in text or "xiaomi" in text:
-        return scrape_trendyol("Xiaomi Mi Air Fryer")
-
     else:
-        raise ValueError("Desteklenmeyen platform.")
+        raise ValueError("Sadece Amazon, Hepsiburada veya Trendyol linkleri destekleniyor.")
